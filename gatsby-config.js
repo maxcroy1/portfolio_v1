176281d522502f6011886cf2eb83003f399f135a
maxcroy1 <max.croy1@gmail.com>
2021-11-12 23:41:14 -0500

    Added experience to content directory, and finished rough layout of website

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,5 +33,13 @@ module.exports = {
       },
       __key: "pages",
     },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "content",
+        path: `./content/`,
+      },
+      __key: "content",
+    },
   ],
 };
--- src/components/sections/experience.js
+++ src/components/sections/experience.js
@@ -1,9 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 
 const Experience = () => {
+  const [activeTabId, setActiveTabId] = useState(0);
+
+  const data = useStaticQuery(graphql`
+    query {
+      jobs: allMdx(
+        sort: { order: DESC, fields: frontmatter___date }
+        filter: { fileAbsolutePath: { ne: "jobs" } }
+      ) {
+        edges {
+          node {
+            id
+            frontmatter {
+              company
+              date
+              location
+              range
+              title
+              url
+            }
+            body
+          }
+        }
+      }
+    }
+  `);
+
+  const jobsData = data.jobs.edges;
+
   return (
     <>
       <h2>Experience</h2>
+      {jobsData &&
+        jobsData.map(({ node }, i) => {
+          const { company } = node.frontmatter;
+          return (
+            <button
+              key={i}
+              isActive={activeTabId === i}
+              onClick={() => setActiveTabId(i)}
+            >
+              {company}
+            </button>
+          );
+        })}
+      {jobsData &&
+        jobsData.map(({ node }, i) => {
+          const { frontmatter, body } = node;
+          const { title, url, company, range } = frontmatter;
+
+          return (
+            <div hidden={activeTabId !== i}>
+              <h3>
+                {title} at{" "}
+                <a href={url} target="_blank" rel="noreferrer">
+                  {company}
+                </a>
+              </h3>
+              <h4>{range}</h4>
+              <MDXRenderer>{body}</MDXRenderer>
+            </div>
+          );
+        })}
     </>
   );
 };
