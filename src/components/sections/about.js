05421f11a24023643c54e99ed8db712c15049fc6
maxcroy1 <max.croy1@gmail.com>
2021-11-25 20:28:15 -0600

    Styled experience buttons

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -5,7 +5,6 @@ import styled from "styled-components";
 const AboutContainer = styled.div`
   height: 100vh;
   position: relative;
-  z-index: 0;
   margin-left: 200px;
 `;
 
--- src/components/sections/experience.js
+++ src/components/sections/experience.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import styled from "styled-components";
 
 const Experience = () => {
   const [activeTabId, setActiveTabId] = useState(0);
@@ -31,41 +32,98 @@ const Experience = () => {
 
   const jobsData = data.jobs.edges;
 
-  return (
-    <>
-      <h2>Experience</h2>
-      {jobsData &&
-        jobsData.map(({ node }, i) => {
-          const { company } = node.frontmatter;
-          return (
-            <button
-              key={i}
-              isActive={activeTabId === i}
-              onClick={() => setActiveTabId(i)}
-            >
-              {company}
-            </button>
-          );
-        })}
-      {jobsData &&
-        jobsData.map(({ node }, i) => {
-          const { frontmatter, body } = node;
-          const { title, url, company, range } = frontmatter;
+  const ExperienceContainer = styled.div`
+    height: 100vh;
+    position: relative;
+    margin-left: 200px;
+  `;
 
-          return (
-            <div hidden={activeTabId !== i}>
-              <h3>
-                {title} at{" "}
-                <a href={url} target="_blank" rel="noreferrer">
-                  {company}
-                </a>
-              </h3>
-              <h4>{range}</h4>
-              <MDXRenderer>{body}</MDXRenderer>
-            </div>
-          );
-        })}
-    </>
+  const ExperienceButtons = styled.div`
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 50%;
+    float: left;
+
+    ul {
+      padding-left: 0;
+    }
+    
+    li {
+      list-style: none;
+      padding: 0;
+      margin-bottom: 10px;
+    }
+
+    .selected {
+      text-decoration: underline;
+    }
+  `;
+
+  const ExperienceContent = styled.div`
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 50%;
+    float: left;
+  `;
+
+  const Employer = styled.button`
+    background-color: #faf2e5;
+    border: none;
+    display: inline;
+    vertical-align: middle;
+    font-size: 1.5em;
+    font-weight: bold;
+    text-transform: uppercase;
+  `;
+
+  return (
+    <ExperienceContainer>
+      <ExperienceButtons>
+        <h2>
+          <u>Experience</u>
+        </h2>
+        <ul>
+          {jobsData &&
+            jobsData.map(({ node }, i) => {
+              const { company } = node.frontmatter;
+              return (
+                <li>
+                  <Employer
+                    key={i}
+                    isActive={activeTabId === i}
+                    onClick={() => setActiveTabId(i)}
+                    className={activeTabId === i ? 'selected' : null}
+                  >
+                    {company}
+                  </Employer>
+                </li>
+              );
+            })}
+        </ul>
+      </ExperienceButtons>
+      <ExperienceContent>
+        {jobsData &&
+          jobsData.map(({ node }, i) => {
+            const { frontmatter, body } = node;
+            const { title, url, company, range } = frontmatter;
+
+            return (
+              <div hidden={activeTabId !== i}>
+                <h3>
+                  {title} at{" "}
+                  <a href={url} target="_blank" rel="noreferrer">
+                    {company}
+                  </a>
+                </h3>
+                <h4>{range}</h4>
+                <MDXRenderer>{body}</MDXRenderer>
+              </div>
+            );
+          })}
+      </ExperienceContent>
+    </ExperienceContainer>
   );
 };
 
