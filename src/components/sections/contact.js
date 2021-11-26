8fd8c703c0906431a6d91471d197545918812066
maxcroy1 <max.croy1@gmail.com>
2021-11-26 15:53:36 -0600

    Added content to Contact section

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -11,6 +11,17 @@ const ContactMessageContainer = styled.div`
   position: relative;
   top: 50%;
   transform: translateY(-50%);
+  width: 450px;
+`;
+
+const EmailButton = styled.a`
+  padding: 1vh 1vw;
+  border: solid black 2px;
+  text-transform: uppercase;
+  text-decoration: none;
+  font-weight: bold;
+  color: black;
+  display: inline-block;
 `;
 
 const Contact = () => {
@@ -18,6 +29,15 @@ const Contact = () => {
     <ContactContainer>
       <ContactMessageContainer>
         <h2>Contact</h2>
+        <p>
+          If you think I'd be a good fit for your team, feel free to reach out!
+          web2 or web3 - I'm always on the lookout for interesting
+          opportunities.
+        </p>
+        <p> Send me an email, and I'll do my best to get back to you.</p>
+        <EmailButton href="mailto:me@maxcroy.com" target="_blank">
+          Get In Touch
+        </EmailButton>
       </ContactMessageContainer>
     </ContactContainer>
   );
